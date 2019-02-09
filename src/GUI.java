ef1836beed7f80482130f044e0aa8a47f5fa8413
Simone Mallia <46264933+simonemallia@users.noreply.github.com>
2019-02-09 21:54:11 +0100

    Update GUI.java

--- src/GUI.java
+++ src/GUI.java
@@ -60,6 +60,8 @@ public class GUI extends JFrame {
 	ImageIcon reinicon = new ImageIcon(getClass().getResource("reinicon.png"));
 	ImageIcon winicon = new ImageIcon(getClass().getResource("winicon.png"));
 	ImageIcon dvaicon = new ImageIcon (getClass().getResource("dvaicon.png"));
+	ImageIcon orisaicon = new ImageIcon (getClass().getResource("orisaicon.png"));
+
 	private JLabel label;
 	private JLabel label_1;
 	private JLabel label_2;
@@ -81,6 +83,11 @@ public class GUI extends JFrame {
 	private JPanel panel_4;
 	private JTable table_2;
 	private JLabel lblSuoniDva;
+	private JMenuItem mntmLicenza;
+	private JScrollPane scrollPane_3;
+	private JPanel panel_5;
+	private JTable table_3;
+	private JLabel lblSuoniOrisa;
 
 	/**
 	 * Launch the application.
@@ -113,7 +120,7 @@ public class GUI extends JFrame {
 	public GUI() throws URISyntaxException, IOException {
 		setTitle("Overwatch Sounds Compilation");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 612, 528);
+		setBounds(100, 100, 632, 549);
 		
 		menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
@@ -127,7 +134,7 @@ public class GUI extends JFrame {
 		JMenuItem mntmCredits = new JMenuItem("Credits");
 		mntmCredits.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				JOptionPane.showMessageDialog(contentPane, "Overwatch Sounds Compilation v0.02a\nSviluppato da Simone Mallia\nGrazie per utilizzare la mia applicazione :)", "Informazioni", JOptionPane.INFORMATION_MESSAGE);
+				JOptionPane.showMessageDialog(contentPane, "Overwatch Sounds Compilation v0.03a\nSviluppato da Simone Mallia\nGrazie per utilizzare la mia applicazione :)", "Informazioni", JOptionPane.INFORMATION_MESSAGE);
 			}
 		});
 		menu.add(mntmCredits);
@@ -147,6 +154,18 @@ public class GUI extends JFrame {
 				}
 			}
 		});
+		
+		mntmLicenza = new JMenuItem("Licenza");
+		mntmLicenza.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				Image iconlicense = Toolkit.getDefaultToolkit().getImage(getClass().getResource("license.png"));
+				License licenza = new License ();
+				licenza.setIconImage(iconlicense);
+				licenza.setLocationRelativeTo(contentPane);
+				licenza.setVisible(true);
+			}
+		});
+		menu.add(mntmLicenza);
 		menu.add(mntmVaiAlSito);
 		
 		contentPane = new JPanel();
@@ -155,7 +174,7 @@ public class GUI extends JFrame {
 		setContentPane(contentPane);
 		
 		panel = new JPanel();
-		panel.setPreferredSize(new Dimension(10, 100));
+		panel.setPreferredSize(new Dimension(10, 110));
 		contentPane.add(panel, BorderLayout.NORTH);
 		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 		
@@ -323,7 +342,7 @@ public class GUI extends JFrame {
 		                       	                        
 							    
 							} catch (ArrayIndexOutOfBoundsException e1) {
-								JOptionPane.showMessageDialog(scrollPane, "Per riprodurre il file clicca sul nome presente nella tabella", "Errore", JOptionPane.ERROR_MESSAGE);
+								JOptionPane.showMessageDialog(scrollPane_1, "Per riprodurre il file clicca sul nome presente nella tabella", "Errore", JOptionPane.ERROR_MESSAGE);
 							} catch (JavaLayerException e1) {
 								e1.printStackTrace();
 							}
@@ -342,7 +361,7 @@ public class GUI extends JFrame {
 		panel_3.add(lblSuoniWinston, BorderLayout.NORTH);
 		
 		scrollPane_2 = new JScrollPane();
-		scrollPane_2.getVerticalScrollBar().setUnitIncrement(16);
+        scrollPane_2.getVerticalScrollBar().setUnitIncrement(16);
 		tabbedPane.addTab("D.va", dvaicon, scrollPane_2, null);
 		
 		panel_4 = new JPanel();
@@ -401,7 +420,7 @@ public class GUI extends JFrame {
 		                       	                        
 							    
 							} catch (ArrayIndexOutOfBoundsException e1) {
-								JOptionPane.showMessageDialog(scrollPane, "Per riprodurre il file clicca sul nome presente nella tabella", "Errore", JOptionPane.ERROR_MESSAGE);
+								JOptionPane.showMessageDialog(scrollPane_2, "Per riprodurre il file clicca sul nome presente nella tabella", "Errore", JOptionPane.ERROR_MESSAGE);
 							} catch (JavaLayerException e1) {
 								e1.printStackTrace();
 							}
@@ -421,6 +440,85 @@ public class GUI extends JFrame {
 		lblSuoniDva.setHorizontalAlignment(SwingConstants.CENTER);
 		panel_4.add(lblSuoniDva, BorderLayout.NORTH);
 		
+		scrollPane_3 = new JScrollPane();
+		scrollPane_3.getVerticalScrollBar().setUnitIncrement(16);
+		tabbedPane.addTab("Orisa", orisaicon, scrollPane_3, null);
+		
+		panel_5 = new JPanel();
+		scrollPane_3.setViewportView(panel_5);
+		panel_5.setLayout(new BorderLayout(0, 0));
+		
+		//Start Orisa table
+		table_3 = new JTable();
+		DefaultTableModel modelorisa = new DefaultTableModel() {
+			@Override
+		    public boolean isCellEditable(int row, int column) {
+		        return false;
+		    }
+		};
+		table_3.setModel(modelorisa);
+    	modelorisa.setColumnIdentifiers(new String [] {"Suoni"});
+    	InputStream inputorisa = getClass().getClassLoader().getResourceAsStream("orisa/masterlistorisa.txt");
+    	InputStreamReader inputorisareader = new InputStreamReader (inputorisa);
+    	BufferedReader brorisa = new BufferedReader (inputorisareader);
+		Object[] roworisa = new Object[1];
+		for (String line4 = brorisa.readLine(); line4 != null; line4 = brorisa.readLine()) {
+		roworisa[0] = line4;
+		modelorisa.addRow(roworisa);
+		}
+		br.close();
+		table_3.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Popup riproduzione = new Popup();		 
+                new Thread(){	
+				public void run() {
+			
+			           try {
+			            	int index = table_3.getSelectedRow();
+			            	int convertrow = table_3.convertRowIndexToModel(index);
+			                String value = modelorisa.getValueAt(convertrow, 0).toString();
+			                AdvancedPlayer audio = new AdvancedPlayer (getClass().getClassLoader().getResourceAsStream("orisa/" + value));			
+							audio.setPlayBackListener(new PlaybackListener() {
+		                        @Override
+		                        public void playbackStarted(PlaybackEvent evt){
+		                        	riproduzione.setLocationRelativeTo(contentPane);
+									riproduzione.setVisible(true);
+									  riproduzione.lblStop.addMouseListener(new MouseAdapter() {
+				    						@Override
+				    						public void mouseClicked(MouseEvent e) {
+				    						audio.stop();
+				    						}
+				    					});
+		                        	
+		                        }
+		                        public void playbackFinished(PlaybackEvent event) {
+		                        	riproduzione.dispose();
+		                        }
+								});   
+		                        audio.play();
+		                       	                        
+							    
+							} catch (ArrayIndexOutOfBoundsException e1) {
+								JOptionPane.showMessageDialog(scrollPane_3, "Per riprodurre il file clicca sul nome presente nella tabella", "Errore", JOptionPane.ERROR_MESSAGE);
+							} catch (JavaLayerException e1) {
+								e1.printStackTrace();
+							}
+
+				}
+		        }.start();                  	        
+		}
+			
+	});
+		
+		panel_5.add(table_3, BorderLayout.CENTER);
+		
+		int suoniorisa = table_3.getRowCount();
+		lblSuoniOrisa = new JLabel("Suoni presenti: " + Integer.toString(suoniorisa));
+		lblSuoniOrisa.setHorizontalAlignment(SwingConstants.CENTER);
+		lblSuoniOrisa.setFont(new Font("Dialog", Font.BOLD, 11));
+		panel_5.add(lblSuoniOrisa, BorderLayout.NORTH);
+		
 		panel_1 = new JPanel();
 		contentPane.add(panel_1, BorderLayout.SOUTH);
 		
@@ -429,6 +527,7 @@ public class GUI extends JFrame {
 		TableRowSorter<DefaultTableModel>filter  = new TableRowSorter<DefaultTableModel> ((DefaultTableModel) table.getModel());
 	    TableRowSorter<DefaultTableModel>filter1 = new TableRowSorter<DefaultTableModel> ((DefaultTableModel) table_1.getModel());
 	    TableRowSorter<DefaultTableModel>filter2 = new TableRowSorter<DefaultTableModel> ((DefaultTableModel) table_2.getModel());
+	    TableRowSorter<DefaultTableModel>filter3 = new TableRowSorter<DefaultTableModel> ((DefaultTableModel) table_3.getModel());
 
 
 		textField.addKeyListener(new KeyAdapter() {
@@ -449,6 +548,11 @@ public class GUI extends JFrame {
 			       table_2.setRowSorter(filter2);
 			       String text2 = textField.getText();
 			       filter2.setRowFilter(RowFilter.regexFilter(text2)); 
+			       
+			       //Orisa
+			       table_3.setRowSorter(filter3);
+			       String text3 = textField.getText();
+			       filter3.setRowFilter(RowFilter.regexFilter(text3)); 
 			}
 		});
 		
@@ -466,6 +570,8 @@ public class GUI extends JFrame {
 				filter.setRowFilter(RowFilter.regexFilter(""));
 			    filter1.setRowFilter(RowFilter.regexFilter(""));
 			    filter2.setRowFilter(RowFilter.regexFilter(""));
+			    filter3.setRowFilter(RowFilter.regexFilter(""));
+
 
 			}
 		});
